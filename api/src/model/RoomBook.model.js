ceffd22a0c00b7b27d96a1f42514015e172ec171
Narvana <adhikarijagdish3@gmail.com>
2024-12-10 19:21:09 +0530

    RoomBook model added

---
+++ api/src/model/RoomBook.model.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const guestSchema= new mongoose.Schema(
+    {
+        GuestName:{
+            type:String,
+            required:[true,'Guest name is required']
+        },
+        GuestContact:{
+            type:String,
+            required:[true,'Guest Contact is required']
+        },
+        GuestIdentity:{
+            type:String,
+            required:[true,'Guest Identity is required']
+        }
+    }
+)
+
+const RoomBookSchema=new mongoose.Schema(
+    {
+        RoomID:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'rooms',
+            required:[true,'Room ID is required']
+        },
+        BookingName:{
+            type:String,
+            required:[true,'Booking Name is required'],
+        },    
+        BookingContact:{
+            type:String,
+            required:[true,'Booking Contact is required'],
+        },
+        CheckIn:{
+            type: Date,
+            required:[true,'Check In Time is required']
+        },
+        CheckOut:{
+            type: Date,
+            required:[true,'Check Out Time is required']
+        },
+        // amount:{}
+        payment:{
+            type:String
+        },
+        Guests:{
+            type:[guestSchema],
+            validate:
+            {
+                validator:function(v){
+                    return v && v.length>0;
+                },
+                message:'Please provide Guest Information',
+            }
+        }
+})
+
+const RoomBook= mongoose.model('RoomBook',RoomBookSchema)
+
+module.exports=RoomBook;
