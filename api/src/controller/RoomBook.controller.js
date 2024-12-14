60041ca67bc9cd17c6ef556653c3b4cac78ca2a8
Narvana <adhikarijagdish3@gmail.com>
2024-12-14 15:16:10 +0530

    getBookRoom getOldBookRoom UpdateBookRoom added in roomBook controller

--- api/src/controller/RoomBook.controller.js
+++ api/src/controller/RoomBook.controller.js
@@ -63,9 +63,13 @@ const BookRoom=async(req,res,next)=>{
 }
 
 const getBookRoom = async(req,res,next)=>{
-    try {
-        
-        const Rooms=await RoomBook.aggregate([
+    try {     
+          const Rooms=await RoomBook.aggregate([
+            {
+                $match:{
+                    OldData:false   
+                }
+            },
                {
                 $lookup:{
                     from:'rooms',
@@ -107,15 +111,15 @@ const getBookRoom = async(req,res,next)=>{
                         payment:1,
                         Guests:1,
                         CheckIn:'$FormattedCheckIn',
-                        CheckOut:'$FormattedCheckOut'
+                        CheckOut:'$FormattedCheckOut',
+                        OldData : 1
                     }
                } 
         ]);
-        
 
         if(Rooms.length === 0)
         {
-            return next(ApiError(400,'No Room Found'));
+            return next(ApiError(400,'No Booked Room Found'));
         }
 
         return next(ApiSuccess(201,Rooms,'Total Booked Rooms'));
@@ -132,7 +136,135 @@ const getBookRoom = async(req,res,next)=>{
     }
 }
 
+const getOldBookRoom = async(req,res,next)=>{
+    try {     
+          const Rooms=await RoomBook.aggregate([
+            {
+                $match:{
+                    OldData:true   
+                }
+            },
+               {
+                $lookup:{
+                    from:'rooms',
+                    localField: 'RoomID',
+                    foreignField: '_id',
+                    as: 'RoomDetails'
+                }
+               },
+               {
+                  $unwind: {
+                     path: '$RoomDetails', 
+                    //  preserveNullAndEmptyArrays: false
+                   }
+               },
+               {
+                  $addFields:
+                    {
+                        FormattedCheckIn: {
+                            $dateToString: {
+                                format: '%Y-%m-%d %H:%M:%S',
+                                date: '$CheckIn',
+                                timezone: 'Asia/Kolkata' // Optional: Specify timezone if required
+                            }
+                        },
+                        FormattedCheckOut: {
+                            $dateToString: {
+                                format: '%Y-%m-%d %H:%M:%S',
+                                date: '$CheckOut',
+                                timezone: 'Asia/Kolkata' // Optional: Specify timezone if required
+                            }
+                        }
+                    }
+               },
+               {
+                    $project:{
+                        RoomNo:'$RoomDetails.roomNum',
+                        BookingName:1,
+                        BookingContact:1,
+                        payment:1,
+                        Guests:1,
+                        CheckIn:'$FormattedCheckIn',
+                        CheckOut:'$FormattedCheckOut',
+                        OldData : 1
+                    }
+               } 
+        ]);
+
+        if(Rooms.length === 0)
+        {
+            return next(ApiError(400,'No Old Booked Room Found'));
+        }
+
+        return next(ApiSuccess(201,Rooms,'Total Old Booked Rooms'));
+        
+    } catch (error) {
+        console.log(
+            {
+            'Internal Serve Error, ' : error.message,
+            error
+            }
+        );
+        // Default error
+        return next(ApiError(500, `Internal Server Error: ${error.message}`)); 
+    }
+}
+
+const updateBookRoom = async(req,res,next)=>{
+    const id=req.query.BookRoomID;
+    
+    if(!id)
+    {
+        return next(ApiError(400, `Please Provide Booked Room ID`)); 
+    }
+
+    try 
+    {
+        const CheckBookRoom = await RoomBook.findById(id);
+        if(!CheckBookRoom)
+        {
+            return next(ApiError(400, `No Booking found regarding provided Room Book ID`));
+        }
+         await RoomBook.findByIdAndUpdate(
+            id,
+            {
+                $set:{
+                    OldData:true
+                }
+            },
+            {new:true}
+        );
+
+        await Room.findByIdAndUpdate(
+            CheckBookRoom.RoomID,
+            {
+                $set:{
+                    roomAvalibility:true
+                }
+            },
+            {new:true}
+        )
+
+   return next(ApiSuccess(200, [] ,'Room UnBooked Successfully'));
+        
+    } catch (error) {
+        console.log(
+            {
+            'Internal Serve Error, ' : error.message,
+            error
+            }
+        );
+        // Default error
+        return next(ApiError(500, `Internal Server Error: ${error.message}`)); 
+    }  
+
+}
+
+
+
 module.exports={
     BookRoom,
-    getBookRoom
+    getBookRoom,
+    getOldBookRoom,
+    updateBookRoom
 }
